68b702c115ef57213bfa749a859f08dd6077ccb7
Vitor Diogo <vitorpdiogo@gmail.com>
2025-07-05 16:35:39 -0300

    fix: css lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "declaration-empty-line-before": null,
     "comment-empty-line-before": null,
     "selector-class-pattern": null,
-    "selector-id-pattern": null
+    "selector-id-pattern": null,
+    "no-descending-specificity": null
   }
 };
